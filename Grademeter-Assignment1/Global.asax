de94a2e30a34cb7d3875b485703047e3c8de60e8
Puneet Kaur <200354592@student.georgianc.on.ca>
2018-10-09 12:30:52 +0530

    Initial code commit

---
+++ Grademeter-Assignment1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Grademeter_Assignment1.MvcApplication" Language="C#" %>
---
+++ Grademeter-Assignment1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Grademeter_Assignment1.Startup))]
+namespace Grademeter_Assignment1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
