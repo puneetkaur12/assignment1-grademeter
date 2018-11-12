13c6b5b05452a6a418ec988aceed7b71ebc5f06a
Puneet Kaur <200354592@student.georgianc.on.ca>
2018-11-12 20:46:29 +0530

    Assignment 2 - Part 2

--- Grademeter-Assignment1/Controllers/StudentsController.cs
+++ Grademeter-Assignment1/Controllers/StudentsController.cs
@@ -14,6 +14,7 @@ namespace Grademeter_Assignment1.Controllers
     {
         private grademeter_databaseEntities db = new grademeter_databaseEntities();
 
+        [AllowAnonymous]
         // GET: Students
         public ActionResult Index()
         {
@@ -36,6 +37,7 @@ namespace Grademeter_Assignment1.Controllers
             return View(student);
         }
 
+        [Authorize]
         // GET: Students/Create
         public ActionResult Create()
         {
@@ -47,6 +49,7 @@ namespace Grademeter_Assignment1.Controllers
         // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
+        [Authorize]
         [ValidateAntiForgeryToken]
         public ActionResult Create([Bind(Include = "StudentID,FirstName,LastName,Age,GradeID")] Student student)
         {
@@ -61,6 +64,7 @@ namespace Grademeter_Assignment1.Controllers
             return View(student);
         }
 
+        [Authorize]
         // GET: Students/Edit/5
         public ActionResult Edit(int? id)
         {
@@ -81,6 +85,7 @@ namespace Grademeter_Assignment1.Controllers
         // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
+        [Authorize]
         [ValidateAntiForgeryToken]
         public ActionResult Edit([Bind(Include = "StudentID,FirstName,LastName,Age,GradeID")] Student student)
         {
@@ -94,6 +99,7 @@ namespace Grademeter_Assignment1.Controllers
             return View(student);
         }
 
+        [Authorize]
         // GET: Students/Delete/5
         public ActionResult Delete(int? id)
         {
@@ -111,6 +117,7 @@ namespace Grademeter_Assignment1.Controllers
 
         // POST: Students/Delete/5
         [HttpPost, ActionName("Delete")]
+        [Authorize]
         [ValidateAntiForgeryToken]
         public ActionResult DeleteConfirmed(int id)
         {
